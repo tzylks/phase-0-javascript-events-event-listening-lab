545a5a53556735a0808127f9ff5b0727b6c82150
TrevorZylks <trevorzylks@gmail.com>
2021-05-20 17:00:28 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
+const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+function addingEventListener() {
+    input.addEventListener('click', clickAlert); 
+}
+
